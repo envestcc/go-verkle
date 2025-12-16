a9d13963495df8857c38768c05ea6dec7646df76
envestcc <chen1233216@hotmail.com>
2025-12-16 16:14:22 +0800

    fix BytesUncompressedTrusted

--- tree_test.go
+++ tree_test.go
@@ -701,7 +701,7 @@ func TestNodeSerde(t *testing.T) {
 	if err := tree.Insert(fourtyKeyTest, testValue, nil); err != nil {
 		t.Fatalf("inserting into key2 failed: %v", err)
 	}
-	origComm := tree.Commit().BytesUncompressed()
+	origComm := tree.Commit().BytesUncompressedTrusted()
 	root := tree.(*InternalNode)
 
 	// Serialize all the nodes
@@ -745,10 +745,10 @@ func TestNodeSerde(t *testing.T) {
 	resRoot.children[64] = resLeaf64
 
 	if !isInternalEqual(root, resRoot) {
-		t.Fatalf("parsed node not equal, %x != %x", root.commitment.BytesUncompressed(), resRoot.commitment.BytesUncompressed())
+		t.Fatalf("parsed node not equal, %x != %x", root.commitment.BytesUncompressedTrusted(), resRoot.commitment.BytesUncompressedTrusted())
 	}
 
-	if resRoot.Commitment().BytesUncompressed() != origComm {
+	if resRoot.Commitment().BytesUncompressedTrusted() != origComm {
 		t.Fatal("invalid deserialized commitment")
 	}
 }
